97d4d52ceec56ef95864bb16bd827f3cff710298
Tenlee Hwang <0936105103@sayhi.tw>
2019-08-12 22:30:36 +0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by 黃彥中 on 2019/8/12.
+//  Copyright © 2019 Ten-lee Hwang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
